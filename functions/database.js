eb1964eca04b00e3f7377f479db05fd528566f56
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 <alejandrojasit@icloud.com>
2022-05-04 16:20:35 -0300

    early040522

--- functions/database.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose')
-let serverAddress = 'mongodb+srv://alejandrorojasit:as050906@miescuelacluster.yxlu2.mongodb.net/MiEscuela?retryWrites=true&w=majority'
-mongoose.connect(serverAddress)
-   .then(db => console.log('DB is connected'))
-   .catch(err => console.error(err))
-
-module.exports = mongoose;
